ee5c860c1c519a512dfaa0d1c9b478465e5e6bb5
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2021-06-18 17:51:56 +0200

    exercise multiples_numbers not yet done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,11 @@
 // define your function here
+const multiplesOf = (number, limit) => {
+    const multiples = []
+    
+    for (let i = 0; i <= limit; i++) {
+        if (i % number === 0) {
+           return multiples.push(i)
+        }
+
+    }
+}
