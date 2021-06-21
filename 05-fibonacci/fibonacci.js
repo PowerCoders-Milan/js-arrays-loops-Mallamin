332738f5699878fdaa946b9a5405f4c1e7f23328
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2021-06-21 12:35:39 +0200

    exercise fibonacci done

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -2,3 +2,22 @@
 const maxNumber = Math.ceil(Math.random()*200);
 let n1 = 0, n2 = 1, nextTerm;
 
+const getFibonacciSequence=(maxNumber)=>{
+  
+
+  if (maxNumber===n2) 
+  {
+    return [n1, n2];
+  } 
+  else 
+  {
+    let nextTerm = getFibonacciSequence(maxNumber - n2);
+    nextTerm.push(nextTerm[nextTerm.length - n2] + nextTerm[nextTerm.length - 2]);
+    return nextTerm;
+  }
+};
+
+ console.log(getFibonacciSequence(9));
+ 
+
+
